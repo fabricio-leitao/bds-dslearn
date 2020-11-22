a3dc8b1fedf8d76873fc7f758a5b469a670c3860
Fabricio Ramos <fabricio-beco@hotmail.com>
2020-11-22 11:44:52 -0300

    Fix: missing associations, getters

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -94,10 +94,6 @@ public class User implements UserDetails, Serializable{
 		return roles;
 	}
 
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
-
 	public List<Notification> getNotifications() {
 		return notifications;
 	}
